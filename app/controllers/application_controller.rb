a8234c5f78c828012c78db8896537c9da4b6e361
reatgcr0wn <reatgcr0wn@gmail.com>
2016-01-18 20:20:31 +0900

    update few files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
-        home_show_path
+  def after_sign_in_path_for(_resource)
+    home_show_path
     end
 
-    private
-        def sign_in_required
-            redirect_to new_user_session_url unless user_signed_in?
-        end
+  private
+
+  def sign_in_required
+    redirect_to new_user_session_url unless user_signed_in?
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,5 @@ class HomeController < ApplicationController
 
   def show
   end
+
 end
