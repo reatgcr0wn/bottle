cb268288a153cb83c1284f56e392dc97b7e15a28
reatgcr0wn <reatgcr0wn@gmail.com>
2016-01-19 04:10:38 +0900

    fix style

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -28,6 +28,8 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
 
+    
+
     respond_to do |format|
       if @message.save
         format.html { redirect_to @message, notice: 'Message was successfully created.' }
