aaa82c78991ea5989aad0cdcbc8b5c4d478dac34
reatgcr0wn <reatgcr0wn@gmail.com>
2016-01-19 05:58:41 +0900

    ある程度めどついた

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,13 @@
 class HomeController < ApplicationController
   before_action :sign_in_required, only: [:show]
   def index
-    @messages = Message.order("created_at DESC").limit(10)
+    # @messages = Message.order("created_at DESC").limit(100)
+    @messages = Message.order("RANDOM()").where(torn: false).first(10)
   end
 
   def show
+    # @messages = Message.order("created_at DESC").limit(100)
+    @messages = Message.where(user_id: current_user.id).limit(100)
   end
 
 end
