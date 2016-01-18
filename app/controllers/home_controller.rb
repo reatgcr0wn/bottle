f369ba66a9ef10d2e74b98a2376bc1e727a090d2
reatgcr0wn <reatgcr0wn@gmail.com>
2016-01-18 18:03:30 +0900

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   before_action :sign_in_required, only: [:show]
   def index
+    @messages = Message.order("created_at DESC").limit(10)
   end
 
   def show
